94db4facffa2f5e415d151d299b7894541734a99
vadwwxz <vadwwxz@gmail.com>
2023-10-15 17:41:56 +0300

    i fix syntax bug, fix bug faced with NullPointerException in Runtime

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,65 +5,46 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
-import com.example.myapplication.R.id.ResetButton
 import com.example.myapplication.R.id.button
 import com.example.myapplication.R.id.countertxt
-import com.example.myapplication.R.id.textView2
-import org.w3c.dom.Text
-
 
 
 class MainActivity : AppCompatActivity() {
-
-    //uploading our variable here:
-    var counter = 0
-    val seeText = findViewById<TextView>(R.id.countertxt)
+    private var counter = 0
+    private lateinit var seeText: TextView
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.run {
-            putString("seeInt", "s")
-        }
         super.onSaveInstanceState(outState)
+        outState.putInt("counterValue", counter)
     }
 
-    override fun onRestoreInstanceState(
-        savedInstanceState: Bundle?,
-        persistentState: PersistableBundle?
-    ) {
-        super.onRestoreInstanceState(savedInstanceState, persistentState)
-           var seeText = savedInstanceState?.getString("seeInt", "s")
-
-
-        seeText.text = countertxt
-        if(savedInstanceState != null){
-            seeText = savedInstanceState.getString("seeInt")
-            seeText.text = seeText.toString()
-        }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val data = savedInstanceState.getInt("counterValue")
+        seeText.text = "Counter: $data"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-           // CONNECTING HERE ALL THINGS OF OUR DESIGN
-
+        seeText = findViewById<TextView>(R.id.countertxt)
         val button = findViewById<Button>(button)
         val resetButton = findViewById<Button>(R.id.ResetButton)
-      //Click button logic
-      button.setOnClickListener() {
-          // when we click this button counter will add 1 number
-          counter++
-          seeText.text = "Counter: $counter"
-      }
-     //Reset button logic
-        resetButton.setOnClickListener() {
-             //when user click the button, counter will just reset
-            counter = 0
+
+        //Click button logic
+        button.setOnClickListener() {
+            // when we click this button counter will add 1 number
+            counter++
             seeText.text = "Counter: $counter"
         }
 
+        //Reset button logic
+        resetButton.setOnClickListener() {
+            //when user click the button, counter will just reset
+            counter = 0
+            seeText.text = "Counter: $counter"
+        }
     }
-
-
-    }
-
+}
