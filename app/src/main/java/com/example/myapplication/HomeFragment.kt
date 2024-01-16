f08125ddaf77e61b4a4a8ba008fe3d9c5b39abf9
StasBd <stasbodnaruk71@gmail.com>
2024-01-16 21:48:35 +0200

    My necessary completed, that thing is fixed! Thank you so much Vadym, for observing me Fragments logic!

---
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,104 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+import com.example.myapplication.databinding.FragmentHomeBinding
+import org.w3c.dom.Text
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+@Suppress("UNREACHABLE_CODE")
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+// successfully added binding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // we don't supposed to touch this block of code
+   _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var counter = 0  // Initialize counter here
+
+        binding.resetButton.setOnClickListener {
+            counter = 0
+            binding.counterText.text = counter.toString()
+        }
+
+        // Add button click listener
+        binding.addButton.setOnClickListener {
+            counter++
+            binding.counterText.text = counter.toString()
+        }
+
+        // Take button click listener
+        binding.takeButton.setOnClickListener {
+            counter--
+            binding.counterText.text = counter.toString()
+        }
+    }
+
+
+
+    override fun onDestroy(){
+        super.onDestroy()
+        _binding = null
+    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
+
+
+
